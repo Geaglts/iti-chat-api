bfedc0e04e649ee6424a6560e6b6aded28b37129
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-06-24 09:55:28 -0500

    Cree la estrategia de jwt

---
+++ utils/auth/strategies/jwt.js
@@ -0,0 +1,25 @@
+const passport = require('passport');
+const { Strategy, ExtractJwt } = require('passport-jwt');
+const boom = require('@hapi/boom');
+
+const User = require('../../../models/user');
+const { config } = require('../../../config');
+
+passport.use(
+  new Strategy({
+    secretOrKey: config.publicJwtSecret,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  }),
+  async function (tokenPayload, done) {
+    try {
+      const user = await User.findById(tokenPayload.userId);
+      if (!user) {
+        return done(boom.unauthorized(), false);
+      }
+      delete user.password;
+      return done(null, user);
+    } catch (error) {
+      done(error);
+    }
+  }
+);
